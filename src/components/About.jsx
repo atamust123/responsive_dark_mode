62c7c08c5bd077fe3f0ffafaec0b34c8bb96a933
Atakan Ayyıldız <atakanayyildiz28@gmail.com>
2023-08-07 17:53:51 +0300

    react-tilt package was corrupted new packages has added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,38 @@
 import { motion } from "framer-motion";
 import React from "react";
-import Tilt from "react-tilt";
+// import Tilt from "react-tilt";
 
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-export const ServiceCard = ({ index, title, icon, desc }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
+// export const ServiceCard = ({ index, title, icon, desc }) => (
+//   <Tilt className='xs:w-[250px] w-full'>
+//     <motion.div
+//       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+//       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+//     >
+//       <div
+//         options={{
+//           max: 45,
+//           scale: 1,
+//           speed: 450,
+//         }}
+//         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+//       >
+//         <img
+//           src={icon}
+//           alt='web-development'
+//           className='w-16 h-16 object-contain'
+//         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+//         <h3 className='text-white text-[20px] font-bold text-center'>
+//           {title}
+//         </h3>
+//       </div>
+//     </motion.div>
+//   </Tilt>
+// );
 
 const ProjectCard = ({ description, imgUrl, title, navigate }) => {
   return (
